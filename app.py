f6d0a8c464147ae7c79cbe531c865a9f9bcd86fa
markiting735-a11y <markiting735@gmail.com>
2026-04-18 22:22:30 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 from groq import Groq
 
@@ -7,23 +6,26 @@ st.set_page_config(page_title="Abubakar's Humanizer", page_icon="✍️")
 st.title("🚀 AI to Human Converter creator Abubakar")
 
 # API Key Connection
-client = Groq(api_key="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")
+client = Groq(api_key="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")
 
 user_text = st.text_area("AI Text Yahan Dalein:", height=250)
 
 if st.button("Humanize Karein"):
     if user_text:
         with st.spinner('Thinking like a human...'):
-            response = client.chat.completi
-                model="llama-3.1-8b-instant",
-                messages=[
-                    {"role": "system", "content": "Rewrite this like a real person sharing a story or info. Use a mix of punchy short sentences and descriptive ones. Avoid typical AI transition words. Throw in a few rhetorical questions and keep the tone conversational, as if you're explaining this to a friend. Make it flow naturally, not like a structured list."},
-                    {"role": "user", "content": user_text}
-                ],
-                temperature=1.3
-            )
-            st.success("Humanized Result:")
-            st.write(response.choices[0].message.content)
+            try:
+                response = client.chat.completions.create(
+                    model="llama-3.1-8b-instant",
+                    messages=[
+                        {"role": "system", "content": "Rewrite this like a real person sharing a story or info. Use a mix of punchy short sentences and descriptive ones. Avoid typical AI transition words. Throw in a few rhetorical questions and keep the tone conversational, as if you're explaining this to a friend. Make it flow naturally, not like a structured list."},
+                        {"role": "user", "content": user_text}
+                    ],
+                    temperature=1.3
+                )
+                st.success("Humanized Result:")
+                st.write(response.choices[0].message.content)
+            except Exception as e:
+                st.error(f"Error a gaya hai: {e}")
     else:
         st.warning("Pehle kuch likhein!")
-      
+        
