3d8dc0fb93bf2641fa55f9d49609f23839d64e06
markiting735-a11y <markiting735@gmail.com>
2026-04-18 16:22:45 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Abubakar's Humanizer", page_icon="✍️")
 st.title("🚀 AI to Human Converter")
 
 # API Key Connection
-client = Groq(api_key="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")
+client = Groq(api_key="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")
 
 user_text = st.text_area("AI Text Yahan Dalein:", height=250)
 
@@ -15,7 +15,7 @@ if st.button("Humanize Karein"):
     if user_text:
         with st.spinner('Thinking like a human...'):
             response = client.chat.completions.create(
-                model="llama3-8b-8192",
+                model="llama-3.1-8b-instant",
                 messages=[
                     {"role": "system", "content": "You are a professional human writer. Rewrite the text to have high perplexity and burstiness. Use natural flow and avoid common AI words to bypass ZeroGPT perfectly."},
                     {"role": "user", "content": user_text}
